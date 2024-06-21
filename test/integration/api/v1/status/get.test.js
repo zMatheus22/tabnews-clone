a7966e02ef44cc55dc580f999a6d1439cf5d3097
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2024-06-21 17:41:56 +0000

    finish `/api/v1/status` endpoint

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
-test("GET para /api/v1/status deve retornar 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+
+  // Verificar a data da consulta.
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+  // Verificar a versão do database.
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  // Verifica a quantidade máxima de conexão do database.
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  // Verifica a quantidade de conexão ativas no database.
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
