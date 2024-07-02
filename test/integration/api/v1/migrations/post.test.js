3ec71654f90f2699c643d303cb2298a99eeb62de
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2024-07-02 18:29:10 +0000

    adds `/api/v1/migrations` endpoint

---
+++ test/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,38 @@
+import database from "infra/database";
+
+async function cleanDataBase() {
+  await database.query("DROP SCHEMA public CASCADE;");
+  await database.query("CREATE SCHEMA public;");
+}
+
+beforeAll(cleanDataBase);
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  const response1Body = await response1.json();
+
+  expect(response1.status).toBe(201);
+
+  expect(Array.isArray(response1Body)).toBe(true);
+
+  expect(response1Body.length).toBeGreaterThan(0);
+  expect(response1Body[0].path).toMatch(`infra/migrations`);
+
+  let name = "";
+  for (let i = 0; i < response1Body.length; i++) {
+    name = response1Body[i].name;
+    expect(response1Body[i].path).toMatch(`infra/migrations/${name}.js`);
+  }
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  const response2Body = await response2.json();
+
+  expect(response2.status).toBe(200);
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
+});
